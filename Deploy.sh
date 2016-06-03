6a1bd7548b0cd6e6ee67853f2abb8a339023f082
John <wei.jiang@winsuntech.cn>
2016-06-03 11:51:48 +0800

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -11,13 +11,13 @@ apt-get update
 # version
 # nodejs: 6.2.0
 # docker: 1.11.2 Reference: https://docs.docker.com/engine/installation/binaries/
-var node_download_path="https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.xz"
-var node_package_name="node-v6.2.0-linux-x64.tar.xz"
-var node_home_path="node-v6.2.0-linux-x64"
+node_download_path="https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.xz"
+node_package_name="node-v6.2.0-linux-x64.tar.xz"
+node_home_path="node-v6.2.0-linux-x64"
 
-var docker_download_path="https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz"
-var docker_package_name="docker-1.11.2.tgz"
-var docker_home_path="docker"
+docker_download_path="https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz"
+docker_package_name="docker-1.11.2.tgz"
+docker_home_path="docker"
 
 #
 # install some essential packages for docker
@@ -47,7 +47,7 @@ then
 fi
 
 tar Jxf $node_package_name
-\mv -r ./$node_home_path/$node_home_path/* /usr/local/
+\mv -f ./$node_home_path/$node_home_path/* /usr/local/
 
 #
 # install docker
@@ -60,7 +60,7 @@ then
 fi
 
 tar zxf $docker_package_name
-\mv -r ./$docker_home_path/* /usr/local/
+\mv -f ./$docker_home_path/* /usr/local/
 
 #systemctl stop docker
 #
