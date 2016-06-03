56c61b99376ab487b315369697b1e5f2f60dc77e
John <wei.jiang@winsuntech.cn>
2016-06-03 11:46:53 +0800

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
 #
+# update apt sourcelist first
+#
+apt-get update
+
+#
 # define all pathnames
 #
 # version
 # nodejs: 6.2.0
 # docker: 1.11.2 Reference: https://docs.docker.com/engine/installation/binaries/
-var nodepath="https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.xz"
-var dockerpath="https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz"
+var node_download_path="https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.xz"
+var node_package_name="node-v6.2.0-linux-x64.tar.xz"
+var node_home_path="node-v6.2.0-linux-x64"
+
+var docker_download_path="https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz"
+var docker_package_name="docker-1.11.2.tgz"
+var docker_home_path="docker"
 
 #
-# install some essential packages
+# install some essential packages for docker
 #
-apt-get update
-apt-get install xz-utils git
+apt-get -y install xz-utils git
+
+#
+# install avahi packages
+#
+apt-get -y install avahi-daemon avahi-utils
 
 #
 # create a new empty folder
@@ -25,18 +39,31 @@ cd tmp
 #
 # install nodejs
 #
-wget $nodepath
+wget $node_download_path
 if [ $? != 0 ]
 then
    echo "Download nodejs package failed!"
    exit 110
 fi
 
-systemctl stop docker
+tar Jxf $node_package_name
+\mv -r ./$node_home_path/$node_home_path/* /usr/local/
 
-nano /lib/systemd/system/avahi-daemon.service
-Restart=always
+#
+# install docker
+#
+wget $docker_download_path
+if [ $? != 0 ]
+then
+   echo "Download nodejs package failed!"
+   exit 110
+fi
 
+tar zxf $docker_package_name
+\mv -r ./$docker_home_path/* /usr/local/
 
-
-```
+#systemctl stop docker
+#
+#nano /lib/systemd/system/avahi-daemon.service
+#Restart=always
+#
